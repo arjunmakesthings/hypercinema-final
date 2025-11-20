e8823ee8424d5caac5d808cf5dd215219d1e0e05
arjun <82059571+arjunmakesthings@users.noreply.github.com>
2025-11-20 14:47:45 -0500

    interim commits.

--- sketch-1.js
+++ sketch-1.js
@@ -2,10 +2,28 @@
 
 let vid1, vid2;
 
+function preload() {
+  //load all media before sketch starts.
+
+  vid1 = createVideo("./assets/media/dad-in.mp4");
+  vid1.hide();
+  vid2 = createVideo("./assets/media/dad-out.mp4");
+  vid2.hide();
+}
+
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(1280, 720);
 }
 
 function draw() {
   background(0);
+
+  vid1.loop(); //by default it loops.
+  vid2.loop(); //by default it loops.
+
+  push(); 
+//   tint(0,0,0,mouseX); 
+//   image(vid1, 0, 0); 
+  pop();
+  image(vid2, 0, 0);
 }
