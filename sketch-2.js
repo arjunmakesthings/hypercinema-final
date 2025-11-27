0b88da71b6b8a9230b1bd0e2691db082e3245baf
arjun <82059571+arjunmakesthings@users.noreply.github.com>
2025-11-26 19:02:35 -0500

    updates

--- sketch-2.js
+++ sketch-2.js
@@ -2,20 +2,32 @@
 
 let cam;
 
-let cw = 1280;
-let ch = 720;
+let my_memories = [];
+let dad_memories = [];
+
+let units = []; //keep track of how many units are on the area.
+
+function preload() {
+  my_memories[0] = createVideo("./assets/media/my-memories/0.mp4");
+  dad_memories[0] = createVideo("./assets/media/dad-memories/0.mp4");
+
+  for (let i = 0; i < my_memories.length; i++) {
+    my_memories[i].hide();
+    dad_memories[i].hide();
+  }
+}
 
 function setup() {
-  cam = createCapture(VIDEO, {flipped:true}, make_canvas);
+  cam = createCapture(VIDEO, { flipped: true }, make_canvas);
   cam.hide();
 
   pixelDensity(1);
-  noStroke();
-}
-function make_canvas(){
-  createCanvas(cam.width, cam.height); 
-}
 
+  noStroke(); 
+}
+function make_canvas() {
+  createCanvas(cam.width, cam.height);
+}
 
 function draw() {
   background(0);
@@ -24,47 +36,30 @@ function draw() {
 
   detect();
 
-  tint(255,50); 
+  tint(255,20);  
   image(cam, 0, 0);
 
-   updatePixels();
+  // updatePixels();
+
+  for (let unit of units) {
+    // unit.show();
+  }
 }
 
-let max_r = 0;
-let max_r_index = 0;
-
-let max_g = 0;
-let max_g_index = 0; 
+let threshold = 200;
+let noisy = 50;
 
 function detect() {
-  //every frame, find the location of the highest colour values.
-
-  max_r = 0;
-  max_r_index = 0;
-  max_g = 0;
-  max_g_index = 0;
-  
-  for (let i = 0; i < cam.pixels.length; i += 4) {
+  for (let i = 0; i < cam.pixels.length; i+=4) {
     let r = cam.pixels[i]; 
     let g = cam.pixels[i+1]; 
+    let b = cam.pixels[i + 2]; 
 
-    if (r>max_r){
-      max_r = r; 
-      max_r_index = i; 
-    }
-
-    if (g > max_g) {
-      max_g = g;
-      max_g_index = i;
+    if (r > threshold && g < noisy && b < noisy) {
+      let pos = get_coordinates(i);
+      rect(pos.x, pos.y, 5, 5);
     }
   }
-
-  //draw rectangle wherever that is. 
-  let pos = get_coordinates(max_r_index); 
-
-  rect (pos.x, pos.y, 50,50); 
-
-  console.log(max_r, max_g);
 }
 
 //helper to convert from pixels array to x, y.
@@ -81,4 +76,16 @@ function get_coordinates(n) {
   return { x, y };
 }
 
-class Unit {}
+//each unit has a media file that it loops, a position on the screen
+
+class Unit {
+  constructor(x, y, file) {
+    this.x = x;
+    this.y = y;
+    this.file = file;
+  }
+
+  show() {
+    image(this.file, this.x, this.y, 50, 50);
+  }
+}
