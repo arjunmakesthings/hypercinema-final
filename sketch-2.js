98ebb1d9599749d1cba38398e8e5ec3633f7d63c
arjun <82059571+arjunmakesthings@users.noreply.github.com>
2025-11-26 19:12:27 -0500

    what i know.

--- sketch-2.js
+++ sketch-2.js
@@ -23,7 +23,7 @@ function setup() {
 
   pixelDensity(1);
 
-  noStroke(); 
+  noStroke();
 }
 function make_canvas() {
   createCanvas(cam.width, cam.height);
@@ -36,28 +36,44 @@ function draw() {
 
   detect();
 
-  tint(255,20);  
+  tint(255, 20);
   image(cam, 0, 0);
 
   // updatePixels();
 
   for (let unit of units) {
-    // unit.show();
+    unit.show();
   }
 }
 
-let threshold = 200;
+let threshold = 170;
 let noisy = 50;
 
+let dis = 100;
+
 function detect() {
-  for (let i = 0; i < cam.pixels.length; i+=4) {
-    let r = cam.pixels[i]; 
-    let g = cam.pixels[i+1]; 
-    let b = cam.pixels[i + 2]; 
+  for (let i = 0; i < cam.pixels.length; i += 4) {
+    let r = cam.pixels[i];
+    let g = cam.pixels[i + 1];
+    let b = cam.pixels[i + 2];
 
     if (r > threshold && g < noisy && b < noisy) {
+      //this is a red pixel.
+
+      //check if this is far away from other reds that we've seen.
       let pos = get_coordinates(i);
-      rect(pos.x, pos.y, 5, 5);
+
+      if (units.length > 0) {
+        for (let i = 0; i < units.length; i++) {
+          let d = dist(pos.x, pos.y, units[i].x, units[i].y);
+
+          if (d > dis) {
+            units.push(new Unit(pos.x, pos.y, my_memories[0]));
+          }
+        }
+      } else {
+        units.push(new Unit(pos.x, pos.y, my_memories[0]));
+      }
     }
   }
 }
@@ -76,13 +92,13 @@ function get_coordinates(n) {
   return { x, y };
 }
 
-//each unit has a media file that it loops, a position on the screen
-
+//each unit has a media file that it loops, a position on the screen, and a size.
 class Unit {
   constructor(x, y, file) {
     this.x = x;
     this.y = y;
     this.file = file;
+    this.file.loop();
   }
 
   show() {
