32ffc8a448e404f2e16bdb78e41e3a369dda83ea
arjun <82059571+arjunmakesthings@users.noreply.github.com>
2025-11-28 11:10:39 -0500

    achieve blob detection.

--- sketch-2.js
+++ sketch-2.js
@@ -1,21 +1,13 @@
 //blob detection; november, 2025.
 
 let cam;
+let col_to_detect = {
+  r: 0,
+  g: 0,
+  b: 0,
+};
 
-let my_memories = [];
-let dad_memories = [];
-
-let units = []; //keep track of how many units are on the area.
-
-function preload() {
-  my_memories[0] = createVideo("./assets/media/my-memories/0.mp4");
-  dad_memories[0] = createVideo("./assets/media/dad-memories/0.mp4");
-
-  for (let i = 0; i < my_memories.length; i++) {
-    my_memories[i].hide();
-    dad_memories[i].hide();
-  }
-}
+let threshold = 10;
 
 function setup() {
   cam = createCapture(VIDEO, { flipped: true }, make_canvas);
@@ -36,53 +28,50 @@ function draw() {
 
   detect();
 
-  tint(255, 20);
+  tint(255, 100);
   image(cam, 0, 0);
-
-  // updatePixels();
-
-  for (let unit of units) {
-    unit.show();
-  }
 }
 
-let threshold = 170;
-let noisy = 50;
-
-let dis = 100;
-
 function detect() {
-  for (let i = 0; i < cam.pixels.length; i += 4) {
-    let r = cam.pixels[i];
-    let g = cam.pixels[i + 1];
-    let b = cam.pixels[i + 2];
+  for (let x = 0; x < cam.width; x++) {
+    for (let y = 0; y < cam.height; y++) {
+      let n = (y * cam.width + x) * 4;
+      //go over every single pixel, and see if it matches colour.
 
-    if (r > threshold && g < noisy && b < noisy) {
-      //this is a red pixel.
+      let pr = cam.pixels[n];
+      let pg = cam.pixels[n + 1];
+      let pb = cam.pixels[n + 2];
 
-      //check if this is far away from other reds that we've seen.
-      let pos = get_coordinates(i);
+      //color difference:
+      let dr = abs(pr - col_to_detect.r);
+      let dg = abs(pg - col_to_detect.g);
+      let db = abs(pb - col_to_detect.b);
 
-      if (units.length > 0) {
-        for (let i = 0; i < units.length; i++) {
-          let d = dist(pos.x, pos.y, units[i].x, units[i].y);
+      strokeWeight(1);
+      stroke(255);
 
-          if (d > dis) {
-            units.push(new Unit(pos.x, pos.y, my_memories[0]));
-          }
-        }
-      } else {
-        units.push(new Unit(pos.x, pos.y, my_memories[0]));
+      if (dr < threshold && dg < threshold && db < threshold) {
+        point(x, y);
       }
     }
   }
 }
 
+function mousePressed() {
+  let n = get_pixel_index(mouseX, mouseY);
+
+  col_to_detect.r = cam.pixels[n];
+  col_to_detect.g = cam.pixels[n + 1];
+  col_to_detect.b = cam.pixels[n + 2];
+}
+
+// helpers:
 //helper to convert from pixels array to x, y.
 function get_pixel_index(x, y) {
   return (y * cam.width + x) * 4;
 }
 
+//helper to convert from x, y to pixel index.
 function get_coordinates(n) {
   let pixel_number = n / 4;
 
@@ -90,18 +79,4 @@ function get_coordinates(n) {
   let y = Math.floor(pixel_number / cam.width);
 
   return { x, y };
-}
-
-//each unit has a media file that it loops, a position on the screen, and a size.
-class Unit {
-  constructor(x, y, file) {
-    this.x = x;
-    this.y = y;
-    this.file = file;
-    this.file.loop();
-  }
-
-  show() {
-    image(this.file, this.x, this.y, 50, 50);
-  }
 }
