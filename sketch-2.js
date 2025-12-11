36df88bf0d589b6ddc6435bf9544e3d96460c72d
arjun <82059571+arjunmakesthings@users.noreply.github.com>
2025-12-10 20:57:57 -0500

    increase size of memories.

--- sketch-2.js
+++ sketch-2.js
@@ -210,7 +210,7 @@ function detect() {
       let avg_y = unit_accumulators[i].sum_y / unit_accumulators[i].count;
 
       //math tells us that area=height=sqrt(area). area for us is the number of pixels in this accumulator object.
-      let avg_size = Math.sqrt(unit_accumulators[i].count) * 4;
+      let avg_size = Math.sqrt(unit_accumulators[i].count) * 6;
       units[i].update(avg_x, avg_y, avg_size);
     }
   }
