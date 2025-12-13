f42ec25adf5a3e03b8656496df665b76c3fa043d
arjun <82059571+arjunmakesthings@users.noreply.github.com>
2025-12-13 16:02:19 -0500

    remove sound.

--- sketch-2.js
+++ sketch-2.js
@@ -330,7 +330,7 @@ function detect() {
       let avg_y = unit_accumulators[i].sum_y / unit_accumulators[i].count;
 
       //math tells us that area=height=sqrt(area). area for us is the number of pixels in this accumulator object.
-      let avg_size = Math.sqrt(unit_accumulators[i].count) * 12;
+      let avg_size = Math.sqrt(unit_accumulators[i].count) * 11;
       units[i].update(avg_x, avg_y, avg_size);
     }
   }
